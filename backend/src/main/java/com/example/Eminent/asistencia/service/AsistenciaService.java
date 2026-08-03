85d99fd2da98d621c7087c67cceaf4641d86ef22
valeriavcgr <valeria3025gon@gmail.com>
2026-08-02 21:36:07 -0500

    [FIX] arreglo de pendiente en asistencia

--- backend/src/main/java/com/example/Eminent/asistencia/service/AsistenciaService.java
+++ backend/src/main/java/com/example/Eminent/asistencia/service/AsistenciaService.java
@@ -42,9 +42,6 @@ public class AsistenciaService {
 
         if (usuario.getRol() == Usuario.Rol.MONITOR) {
             validarMonitorAsignado(eventoId, usuario.getId());
-            if (evento.getEstado() == Evento.Estado.FINALIZADO || evento.getEstado() == Evento.Estado.CANCELADO) {
-                throw new IllegalArgumentException("Ya no tienes acceso a este evento porque finalizó o fue cancelado");
-            }
         }
 
         List<Inscripcion> activas = inscripcionRepo
